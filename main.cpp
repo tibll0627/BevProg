9c91d496dc671c0e11b3ef1ca3d96d8b7b084e5c
tibll0627 <55576659+tibll0627@users.noreply.github.com>
2019-10-17 19:38:47 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,67 +1,31 @@
-#include"std_lib_facilities.h"
+#include "std_lib_facilities.h"
 
+int main(){
+/*cout<<"Success!\n";
+cout<<"Success!\n";
+cout<<"Success" << "!\n";
+cout<<"Success!"<<'\n';
+int res =7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+vector <int> v(10); v[5] = 7; if (v[5]=7) cout << "Success!\n";
+bool cond = true; if (cond) cout << "Success!\n"; else cout << "Fail!\n";
+bool c = true; if (c) cout << "Success!\n"; else cout << "Fail!\n";
+string s = "ape"; bool c =true; if (c) cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+string s = "fool"; if (s=="fool") cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<v.size(); i++) cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<=v.size(); i++) cout << "Success!\n";
+string s = "Success!"; for (int i=0; i<8; i++) cout << s[i];
+if (true) cout << "Success!\n"; else cout << "Fail!\n";
+int x = 2000; int c = x; if (c==2000) cout << "Success!\n";
+string s = "Success!\n"; for (int i=0; i<8;i++) cout << s[i];
+vector <int> v(5); for (int i=0; i<=v.size(); i++) cout << "Success!\n";
+int i=0; int j = 9; while (i<10){ i++; if (j<i) cout << "Success!\n";}
+int x = 2; double d =4.5; if (d==2*x+0.5) cout << "Success!\n";
+string s = "Success!"; for (int i=0; i<=7; i++) cout << s[i];
+int i=0; int j=9; while (i<10){ i++; if (j<i) cout << "Success!\n";} */
+int x = 4; double d = 5/x-2; if (d==(x-2+0.5)) cout << "Success!\n";
+cout <<"Success!\n";
 
-using namespace std;
-int main()
-{
-
-double a=0,b=0,kicsi,nagy ;
-    const double m_cm = 100,in_cm = 2.54,ft_in = 12;
-    bool tarolo=true;
-    double Sum=0;
-    int db=0;
-    string mertekegyseg;
-    vector<double>v ;
-    char c;
-while(cin>>a>>mertekegyseg)
-{
-
-    if (mertekegyseg == "m"){
-    a = a;
-  }else if (mertekegyseg == "cm"){
-    a = a/m_cm;
-  }else if (mertekegyseg == "in"){
-    a = in_cm*a/m_cm;
-  }else if (mertekegyseg == "ft"){
-    a = ft_in*in_cm*a/m_cm;
-  }
-  else
-  {
-    cout<<"Illegal unit!"<<endl;
-  }
-  v.push_back(a);
-  Sum=Sum+a;
-
-if(tarolo)
-{
-    kicsi=a;
-    nagy=a;
-    tarolo=false;
-}
-
-if(a<kicsi)
-{
-    cout<<"the smallest so far"<<endl;
-    kicsi =a;
-
-}
-else if(a>nagy)
-{
-    cout<<"the largest so far"<<endl;
-    nagy=a;
-}
-db++;
-}
-cout<<"db= "<<db<<endl;
-cout<<"The smaller value is: "<<kicsi<<"m"<<endl;
-cout<<"the larger value is:  "<<nagy<<"m"<<endl;
-cout<<"Sum of values: "<<Sum<<endl;
-sort(v);
-
- cout<<"Values: ";
-for(int i=0;i<v.size();i++)
-
-    cout<<v[i]<<",";
-
-
+return 0;
 }
